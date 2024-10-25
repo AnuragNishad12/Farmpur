ed60a9cd1f4503208458a7c145ad6cbc490afbdc
Anurag Nishad <anuktsksanurag@example.com>
2024-10-25 22:59:27 +0530

    This is 2nd commit

--- app/src/main/java/com/example/farmpur/GetStarted.kt
+++ app/src/main/java/com/example/farmpur/GetStarted.kt
@@ -9,17 +9,20 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 
 @Composable
+@Preview
 fun GetStartedActivity(){
 
    Box(modifier = Modifier
@@ -32,7 +35,10 @@ fun GetStartedActivity(){
            Text(text = "Welcome To Farmpur", fontSize = 20.sp)
             Spacer(modifier = Modifier.height(20.dp))
            Image(painter = painterResource(id = R.drawable.getstarted), contentDescription ="Alt")
-           Button(onClick = { /*TODO*/ }) {
+           Button(onClick = { /*TODO*/ },colors = ButtonDefaults.buttonColors(
+               containerColor =Color(0xFF196F3D),
+               contentColor = Color.White
+           )) {
                Text(text = "Get Started", fontSize = 30.sp)
                
            }
